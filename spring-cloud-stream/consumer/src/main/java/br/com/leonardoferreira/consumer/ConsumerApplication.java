1990aa9a66c2dc46614f06c8ce8cb3080891547f
Leonardo Ferreira <mail@leonardoferreira.com.br>
2019-09-15 18:23:35 -0300

    producer & consumer

--- spring-cloud-stream/consumer/src/main/java/br/com/leonardoferreira/consumer/ConsumerApplication.java
+++ spring-cloud-stream/consumer/src/main/java/br/com/leonardoferreira/consumer/ConsumerApplication.java
@@ -1,9 +1,12 @@
 package br.com.leonardoferreira.consumer;
 
+import br.com.leonardoferreira.consumer.channel.CreatedUserChannel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 
 @SpringBootApplication
+@EnableBinding(CreatedUserChannel.class)
 public class ConsumerApplication {
 
     public static void main(String[] args) {
--- spring-cloud-stream/producer/src/main/java/br/com/leonardoferreira/producer/ProducerApplication.java
+++ spring-cloud-stream/producer/src/main/java/br/com/leonardoferreira/producer/ProducerApplication.java
@@ -1,9 +1,12 @@
 package br.com.leonardoferreira.producer;
 
+import br.com.leonardoferreira.producer.channel.CreatedUserChannel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 
 @SpringBootApplication
+@EnableBinding(CreatedUserChannel.class)
 public class ProducerApplication {
 
     public static void main(String[] args) {
